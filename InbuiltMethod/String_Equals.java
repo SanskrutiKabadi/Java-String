8f32aba6f38896d1ddec6aeeea3ba27200f2d418
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-03-15 07:30:38 +0530

    String_Equals Method

---
+++ InbuiltMethod/String_Equals.java
@@ -0,0 +1,23 @@
+import java.io.*;
+class StringEqual{
+	public static void main(String[] args)throws Exception{
+	
+		String str1,str2;
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter the first string:");
+		str1=br.readLine();
+		System.out.println("Enter the second string:");
+        	str2=br.readLine();
+		System.out.println(str1.equals(str2));	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
