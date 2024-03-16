da29955cf23e09a4edbaf8a4442c41ea581d3345
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-03-16 08:05:48 +0530

    StringLength

---
+++ InbuiltMethod/String_Length.java
@@ -0,0 +1,21 @@
+import java.io.*;
+class StringLength{
+        public static void main(String[] args)throws Exception{
+
+                String str;
+                BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+                System.out.println("Enter the string:");
+                str=br.readLine();
+                System.out.println(str.length());
+
+
+
+
+
+
+
+
+
+        }
+
+}
