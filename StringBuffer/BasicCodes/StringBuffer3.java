327bf7ad4ff04aa8f7d67059dd62709747635566
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-03-27 09:20:06 +0530

    StringBuffer

---
+++ StringBuffer/BasicCodes/StringBuffer3.java
@@ -0,0 +1,16 @@
+class StringBuffer3{
+        public static void main(String[] args){
+		String str1="Sanskruti";
+		String str2=new String("Kabadi");
+		StringBuffer str3=new StringBuffer("Sits");
+        	StringBuffer str4=str3.append(str1);
+        	System.out.println(str1);
+        	System.out.println(str2);
+       		System.out.println(str3);
+       		System.out.println(str4);
+   
+
+        }
+
+
+}
