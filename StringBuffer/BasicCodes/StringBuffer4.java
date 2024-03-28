088d6a9942e71d6c43eba5027226e800fd418d8a
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-03-28 10:04:36 +0530

    StringBuffer

---
+++ StringBuffer/BasicCodes/StringBuffer4.java
@@ -0,0 +1,17 @@
+class StringBuffer4{
+        public static void main(String[] args){
+        StringBuffer str1=new StringBuffer( );
+
+        System.out.println(str1.capacity());
+	str1.append("Kabadi");
+        System.out.println(str1.capacity());
+	System.out.println(str1);
+
+   
+
+
+        }
+
+
+}
+
