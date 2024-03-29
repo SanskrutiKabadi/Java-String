82f90c4dff4e5d3eda8ac0bb0a027770606e0f15
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-03-29 08:42:26 +0530

    Delete

---
+++ String Buffer in-built Methods/Delete.java
@@ -0,0 +1,9 @@
+class Delete{
+        public static void main(String[] args){
+                StringBuffer str1=new StringBuffer("sans");
+                System.out.println(str1);
+                str1.delete(2,5);
+                System.out.println(str1);
+        }
+
+}
