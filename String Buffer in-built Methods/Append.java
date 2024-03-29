a4458d936ba8935e2fae477fe7a7cef6d68e1c56
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-03-29 08:41:59 +0530

    Append

---
+++ String Buffer in-built Methods/Append.java
@@ -0,0 +1,9 @@
+class Append{
+        public static void main(String[] args){
+                StringBuffer str1=new StringBuffer("sans");
+                System.out.println(str1);
+                str1.append("Kabadi");
+                System.out.println(str1);
+        }
+
+}
