a5d60d8d2add0e69e0d3821ba30a9a6c09b3b17a
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-03-30 19:27:35 +0530

    Reverse

---
+++ Reverse.java
@@ -0,0 +1,9 @@
+class Reverse{
+        public static void main(String[] args){
+                StringBuffer str1=new StringBuffer("sanskruti");
+                System.out.println(str1);
+                str1.reverse();
+                System.out.println(str1);
+        }
+
+}
