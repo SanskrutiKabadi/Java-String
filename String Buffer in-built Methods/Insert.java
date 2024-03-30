ec574fbe4d7bf37e9c582f918500d62f0f2bdea5
SanskrutiKabadi <158191637+SanskrutiKabadi@users.noreply.github.com>
2024-03-30 19:27:10 +0530

    Insert

---
+++ String Buffer in-built Methods/Insert.java
@@ -0,0 +1,9 @@
+class Insert{
+        public static void main(String[] args){
+                StringBuffer str1=new StringBuffer("sans");
+                System.out.println(str1);
+                str1.insert(3,"Kabadi");
+                System.out.println(str1);
+        }
+
+}
